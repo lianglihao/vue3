491f0e4f721167771a7b52e17313c4b088148b51
lianglihao <807478609@qq.com>
2019-02-25 15:24:18 +0800

    基础学习完毕

---
+++ vue3-demo/vue.config.js
@@ -0,0 +1,31 @@
+const package = require("./package.json")
+
+module.exports = {
+    publicPath: '/', // 根路径
+    outputDir: 'dist', // 构建输出目录
+    assetsDir: 'assets', // 静态资源目录(js,css,img,fonts)
+    lintOnSave: false, // 是否开启eslint保存检测，有效值：true || false || error
+    devServer: {
+        open: true, // 当项目运行时，是否自动打开浏览器
+        host: 'localhost',
+        port: 8081, //端口号
+        https: false,
+        hotOnly: false,
+        proxy: {
+            // 配置跨域
+            'api': {
+                target: 'http//localhost:5000/api/',
+                ws: true,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        },
+        before: (app) =>{ //提供在服务器内部的所有其他中间件之前执行自定义中间件的能力
+            app.get("/api/users", (req,res) => {
+                res.json(package)
+            })
+        }
+    }
+}
